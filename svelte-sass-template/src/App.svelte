7a200d05d7422972ae43ac12c9114c18b21d4b85
ハムザ ラフィーク <38761754+RafikHamza@users.noreply.github.com>
2024-10-08 23:56:18 +0900

    Add files via upload

---
+++ svelte-sass-template/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let name;
+</script>
+
+<!-- use lang="sass" for SASS -->
+<style lang="scss">
+  $color: red;
+
+  div {
+    > p {
+      background: green;
+      color: white;
+    }
+  }
+
+  h1 {
+    color: $color;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+
+<div>
+  <p>SASS is working!</p>
+</div>
